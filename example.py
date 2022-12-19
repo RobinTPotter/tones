c0ad07444fb8854f4d661387da03362557d38ab2
robin@home <robin@home>
2022-12-19 13:56:53 +0000

    refined order of import in exaple

--- example.py
+++ example.py
@@ -1,10 +1,10 @@
-
-import threading
-import play
 import pygame
 pygame.mixer.set_num_channels(100)
 
-t1 = threading.Thread(None,play.play, kwargs={"beat":0.2})
-t2 = threading.Thread(None,play.play, kwargs={"song":"O5BGB.A.Fs.G.GsGsGs.GGG.FsFsFs.EGEAE.", "beat":0.2})
+import threading
+import play
+
+t1 = threading.Thread(None,play.play, kwargs={"beat":0.3})
+t2 = threading.Thread(None,play.play, kwargs={"song":"O5BGB.A.Fs.G.GsGsGs.GGG.FsFsFs.EGEAE.", "beat":0.3})
 
 t1.start();t2.start()
