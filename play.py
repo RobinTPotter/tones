c1436590a2f104d1ae4019b54eefb1d746fe68be
Potter <Robin.Potter@experian.com>
2023-01-27 11:00:18 +0000

    alts

--- play.py
+++ play.py
@@ -2,8 +2,8 @@ import time
 import re
 import load 
 
-def play(song="O6GEG.Fs.Ds.E.DDD.CsCsCs.CCC.O5BeceB", beat=0.5):
-    song = re.split("(O\d|[A-Ga-g]s|[A-Ga-g])",song)
+def play(song="8O6GEG.Fs.Ds.E.DDD.CsCsCs.CCC.O5BeceB", beat=0.5, length=4):
+    song = re.split("(\d{1,2}|O\d|[A-Ga-g]s|[A-Ga-g])",song)
     song = [s for s in song if s is not ""]
     octave = 3
     for n in song:
@@ -11,6 +11,7 @@ def play(song="O6GEG.Fs.Ds.E.DDD.CsCsCs.CCC.O5BeceB", beat=0.5):
         if n[0]=="O":
             octave = int(n[1])
             continue
+        if isnumeric(n): length=n
         small = n[0].islower()
         sharp = 1 if len(n)==2 and n[1]=="s" else 0
         note = n[0].upper() + ("s" if sharp else "")
@@ -18,7 +19,7 @@ def play(song="O6GEG.Fs.Ds.E.DDD.CsCsCs.CCC.O5BeceB", beat=0.5):
         if note!=".":
             ding = f"{note}{octave + (1 if small else 0)}"
             #print(f"ding {ding}")
-            load.wav[ding].play()
-        time.sleep(beat)
+            load.wav[ding].play() #play for length
+        time.sleep(beat/length)
 
 
